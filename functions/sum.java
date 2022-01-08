5e8d83f8bf86d687a452accc6c332d2a724420ca
venkateshsandupatla <venkateshsandupatla03@gmail.com>
2022-01-08 14:00:02 +0530

    sum2

--- functions/sum.java
+++ functions/sum.java
@@ -4,7 +4,23 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
+
+    }
+
+    // with return type in this function we gev return type as int that means
+    // whenever the function is called the function holds a integer after the
+    // execution of function
+
+    static int sum2() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number two: ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        return sum;
     }
 
     static void sum() {
