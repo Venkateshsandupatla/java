da6cf51c63d612edc7979d6f391a4db5960a11e9
venkatesh <venkateshsandupatla03@gmail.com>
2022-02-03 16:57:56 +0530

    search

---
+++ twodarraysearch/search2darray.java
@@ -0,0 +1,20 @@
+package twodarraysearch;
+
+public class search2darray {
+    
+    public static void main(String[] args) {
+        int[][] matrix = {
+                            {1,2,3},
+                            {5,0,8}
+        } ;
+        
+        int target = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j]==target) {
+                    System.out.println(i +" "+ j);
+                }
+            }
+        }
+    }
+}
