a7bd808cc118850fed45c70f5d545082f20076b7
venkatesh <venkateshsandupatla03@gmail.com>
2022-04-18 16:25:06 +0530

    first

--- hello.java
+++ hello.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class hello {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
         System.out.println("hello world this is venky!!");
     }
 }
