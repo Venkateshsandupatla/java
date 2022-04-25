e35f3f7ec8909bf7c7cdfaf6a22dfdb2c6162345
venkatesh <venkateshsandupatla03@gmail.com>
2022-04-25 02:15:08 +0000

    import

---
+++ Oops/src/oop2/greeting.java
@@ -0,0 +1,12 @@
+package oop2;
+
+import static oop2.message.message; // this means we are importing other files function to this file
+
+public class greeting {
+    public static void main(String[] args) {
+        System.out.println("hi i, venky , how are you?");
+        message(); // this one is from another file -> messege file
+
+    }
+
+}
---
+++ Oops/src/oop2/message.java
@@ -0,0 +1,12 @@
+package oop2;
+
+public class message {
+    public static void main(String[] args) {
+        message();
+    }
+
+    // we created a function , now lets access this functon from greeting file
+    public static void message() {
+        System.out.println("Java is awesome");
+    }
+}
